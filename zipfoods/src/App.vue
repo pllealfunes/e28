dc417a09e83a6aa533a00a2d261e3ac75b71d337
Paula Leal <paula61395@gmail.com>
2020-11-02 13:18:28 -0500

    removed ds store

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img
+      alt="ZipFoods logo"
+      id="logo"
+      src="@/assets/images/zipfoods-logo.png"
+    />
+    <p>
+      ZipFoods is your one-stop-shop for convenient online grocery shopping in
+      the greater Boston area.
+    </p>
+    <show-featured :category="'snacks'"> </show-featured>
+    <show-products></show-products>
+  </div>
+</template>
+
+<script>
+import ShowProducts from "@/components/ShowProducts.vue";
+import ShowFeatured from "@/components/ShowFeatured.vue";
+export default {
+  name: "App",
+  components: {
+    "show-products": ShowProducts,
+    "show-featured": ShowFeatured,
+  },
+};
+</script>
+
+<style lang='scss'>
+@import "@/assets/scss/zipfoods.scss";
+</style>
