1daaf90160566b3df9c0d6c93fdde69d03958c7a
Paula Leal <paula61395@gmail.com>
2021-05-29 21:47:14 -0400

    made a word scramble game

---
+++ word-scramble/app.js
@@ -0,0 +1,62 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        playerName: '',
+        startGame: false,
+        mysteryWord: '',
+        hint: '',
+        guess: '',
+        result: false,
+        words: [
+            ['apple', 'Sometimes red, sometimes delicious'],
+            ['washington', 'Rushmore’s left edge'],
+            ['pumpkin', 'Halloween’s favorite fruit'],
+            ['football', 'Play with your hands or feet, depending on your location']
+        ],
+        num: null,
+        message: null,
+        scrambledWord: '',
+        win: false,
+        lose: true
+    },
+    methods: {
+        submitName() {
+            this.startGame = true;
+            this.num = this.words[Math.floor((Math.random() * this.words.length))];
+            this.mysteryWord = this.num[0];
+            this.hint = this.num[1];
+        },
+        submitGuess() {
+            this.result = true;
+            if (this.guess === this.mysteryWord) {
+                this.message = true;
+                this.win = true;
+            } else if (this.guess != this.mysteryWord) {
+                this.message = false;
+                this.lose = true;
+            } else {
+                this.message = null;
+            }
+        },
+        resetGame() {
+            this.mysteryWord = '';
+            this.hint = '';
+            this.guess = '';
+            this.result = false;
+            this.num = this.words[Math.floor((Math.random() * this.words.length))];
+            if (this.num[0] == this.mysteryWord) {
+                this.num = this.words[Math.floor((Math.random() * this.words.length))];
+            } else {
+                this.mysteryWord = this.num[0];
+                this.hint = this.num[1];
+            }
+        }
+    },
+    computed: {
+        displayMysteryWord() {
+            let mystery = this.mysteryWord.split('');
+            this.scrambledWord = mystery.sort(() => Math.random() - 0.5);
+            return this.scrambledWord.join('');
+        }
+    }
+});
